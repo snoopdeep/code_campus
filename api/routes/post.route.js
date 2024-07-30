6bbc69cfe793c54e2e5a72bcda78801ddce3b516
Deepak Singh <snoopdeep7@gmail.com>
2024-07-30 12:08:10 +0530

    add api route for posts

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,7 +1,9 @@
 import express from "express";
 import { verifyToken } from "../util/verifyUser.js";
 import { create } from "../controllers/post.controller.js";
+import { getposts } from "../controllers/post.controller.js";
 const router=express.Router();
 
 router.post('/create',verifyToken,create);
+router.get('/getposts',getposts);
 export default router;
